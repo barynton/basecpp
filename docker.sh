2433ba88280de14fbdc04e22f1e09dcbf85adb83
Anton <>
2023-06-25 22:57:59 +0200

    Updated

--- docker.sh
+++ docker.sh
@@ -2,26 +2,22 @@
 set -xe
 
 source_dir=$PWD
-container=ubuntu_builder
+container=ubuntu_builder_container
 image=ubuntu_builder
 
 function docker_create {
     docker build -t $image .
-}
-
-function docker_run {
     set +e
     docker container stop $container
     docker container rm $container
     set -e
-    docker run -it -v $source_dir:$source_dir --name $container $image
-}
-
-function docker_cmd {
-    docker start -i $container
+    docker run -dit -v $source_dir:$source_dir --name $container $image
 }
 
 function docker_build {
+    if ! docker inspect "$container" >/dev/null 2>&1; then
+        docker_create
+    fi
     user=`id -u`:`id -g`
     docker start $container
     build_type=Debug
@@ -29,7 +25,26 @@ function docker_build {
         build_type=$1
     fi 
     docker exec $container /bin/bash -c "cd $source_dir && $source_dir/build.sh $build_type"
-    # docker exec $container /bin/bash -c "chown -hR $user $source_dir/build_$build_type/ && chmod -R +r $source_dir/build_$build_type/"
+}
+
+function docker_test_run {
+    user=`id -u`:`id -g`
+    docker start $container
+    build_type=Debug
+    if [ "$1" != "" ] ; then
+        build_type=$1
+    fi 
+    docker exec $container /bin/bash -c "cd '$source_dir/build_$build_type' && ctest"
+}
+
+function docker_test_coverage {
+    user=`id -u`:`id -g`
+    docker start $container
+    docker exec $container /bin/bash -c "cd $source_dir && $source_dir/coverage.sh"
+}
+
+function docker_bash {
+    docker exec -it $container /bin/bash
 }
 
 docker_$1 $2 $3
